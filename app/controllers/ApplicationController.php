d3b7f6fa67d06e19ebc7bd7c41dec6c194916c84
raquel-patino <r.patino.navarro@gmail.com>
2025-02-12 12:46:46 +0100

    subida de archivos creados para trabajar en ellos, no funcional aun

--- app/controllers/ApplicationController.php
+++ app/controllers/ApplicationController.php
@@ -1,10 +1,29 @@
 <?php
-
+require_once __DIR__ . '/../lib/base/Controller.php';// probablemente esta ruta no es correcta
+require_once __DIR__ . '/../app/models/Taskmodel.php';
 /**
  * Base controller for the application.
  * Add general things in this controller.
  */
-class ApplicationController extends Controller 
+class ApplicatonController extends Controller 
 {
-	
+
+    private $taskModel;
+
+    public function __construct()
+    {
+        $this->taskModel= new Taskmodel("data_tasks.json");
+    }
+
+function showDataAction(){
+
+    $tasks =$this->taskModel->fetchAll();
+
+    $this->view->tasks= $tasks;
+
+
 }
+
+}
+	
+
---
+++ app/models/Taskmodel.php
@@ -0,0 +1,99 @@
+<?php
+
+class Taskmodel{
+
+protected $filePath= "";
+protected $data= [];
+
+function __construct($file)
+{
+    
+    $this->filePath= __DIR__ . '/../data/' . $file;
+    $this->loadData();
+
+}
+
+protected function loadData()
+{
+    if (is_file($this->filePath)) {
+        $json = file_get_contents($this->filePath);
+        $this->data = json_decode($json, true);
+        
+        if ($this->data === null) {
+            die("Error al leer JSON: " . json_last_error_msg());
+        }
+    } else {
+        die("No se encontró el archivo JSON: " . $this->filePath);
+    }
+}
+
+public function fetchAll()
+    {
+        
+        return $this->data;
+    }
+
+/*
+<?php
+
+class JsonModel
+{
+    protected $_filePath = "";
+    protected $_data = array();
+
+    public function __construct($file)
+    {
+        $this->_filePath = __DIR__ . '/../data/' . $file;
+        $this->loadData();
+    }
+
+    protected function loadData()
+    {
+        if (is_file($this->_filePath)) {
+            $json = file_get_contents($this->_filePath);
+            $this->_data = json_decode($json, true) ?? [];
+        } else {
+            $this->_data = [];
+        }
+    }
+
+    protected function saveData()
+    {
+        file_put_contents($this->_filePath, json_encode($this->_data, JSON_PRETTY_PRINT));
+    }
+
+    public function save($data)
+    {
+        if (!isset($data['id'])) {
+            $data['id'] = $this->generateId();
+            $this->_data[] = $data;
+        } else {
+            foreach ($this->_data as &$item) {
+                if ($item['id'] == $data['id']) {
+                    $item = array_merge($item, $data);
+                    break;
+                }
+            }
+        }
+        $this->saveData();
+        return $data['id'];
+    }
+
+    protected function generateId()
+    {
+        $ids = array_column($this->_data, 'id');
+        return empty($ids) ? 1 : max($ids) + 1;
+    }
+
+    public function fetchAll()
+    {
+        return $this->_data;
+    }
+}
+    */
+
+
+}
+
+
+?>
