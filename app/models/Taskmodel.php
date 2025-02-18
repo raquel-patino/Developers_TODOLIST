9ffc7ca1c59d7338f2655d2229b06bb7604db9f9
LenaPradoP <lena@atajo.es>
2025-02-18 11:07:43 +0100

    En Taskmodel, actualizo $filePath en constructor y añado saveData()

--- app/models/Taskmodel.php
+++ app/models/Taskmodel.php
@@ -8,7 +8,7 @@ protected $data= [];
 function __construct($file)
 {
     
-    $this->filePath= __DIR__ . '/../data/' . $file;
+    $this->filePath= ROOT_PATH . '/data/'. $file;
     $this->loadData();
 
 }
@@ -33,67 +33,13 @@ public function fetchAll()
         return $this->data;
     }
 
-/*
-<?php
 
-class JsonModel
-{
-    protected $_filePath = "";
-    protected $_data = array();
-
-    public function __construct($file)
+    protected function saveData() 
     {
-        $this->_filePath = __DIR__ . '/../data/' . $file;
-        $this->loadData();
+        return file_put_contents($this->filePath, json_encode($this->data, JSON_PRETTY_PRINT))!== false;
     }
-
-    protected function loadData()
-    {
-        if (is_file($this->_filePath)) {
-            $json = file_get_contents($this->_filePath);
-            $this->_data = json_decode($json, true) ?? [];
-        } else {
-            $this->_data = [];
-        }
-    }
-
-    protected function saveData()
-    {
-        file_put_contents($this->_filePath, json_encode($this->_data, JSON_PRETTY_PRINT));
-    }
-
-    public function save($data)
-    {
-        if (!isset($data['id'])) {
-            $data['id'] = $this->generateId();
-            $this->_data[] = $data;
-        } else {
-            foreach ($this->_data as &$item) {
-                if ($item['id'] == $data['id']) {
-                    $item = array_merge($item, $data);
-                    break;
-                }
-            }
-        }
-        $this->saveData();
-        return $data['id'];
-    }
-
-    protected function generateId()
-    {
-        $ids = array_column($this->_data, 'id');
-        return empty($ids) ? 1 : max($ids) + 1;
-    }
-
-    public function fetchAll()
-    {
-        return $this->_data;
-    }
-}
-    */
 
 
 }
-
 
 ?>
