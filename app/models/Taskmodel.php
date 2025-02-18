e4d6ed6296d050637c20e8e358f1d87703349246
LenaPradoP <lena@atajo.es>
2025-02-18 11:09:58 +0100

    Añado generateId() en Taksmodel

--- app/models/Taskmodel.php
+++ app/models/Taskmodel.php
@@ -33,7 +33,12 @@ public function fetchAll()
         return $this->data;
     }
 
-
+    protected function generateId() 
+    {
+        $ids = array_column($this->data, 'id');
+        return empty($ids) ? 1 : max($ids) + 1;
+    }
+    
     protected function saveData() 
     {
         return file_put_contents($this->filePath, json_encode($this->data, JSON_PRETTY_PRINT))!== false;
